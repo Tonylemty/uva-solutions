a76b0cd54a3e372b2783005350f2c885e47458d3
Tonylemty <chengshinchung31@gmail.com>
2024-06-17 15:59:21 +0800

    Hashmat the Brave Warrior

--- 50-stars/uva10055.c
+++ 50-stars/uva10055.c
@@ -20,3 +20,22 @@ int main() {
     }
 
     return 0;
+}
+
+/* 另一種解法
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   
+    long long soldiers, opponents;
+
+    while (scanf(" %lld %lld", &soldiers, &opponents) != EOF) {
+        long long max = soldiers > opponents ? soldiers : opponents;
+        long long min = soldiers < opponents ? soldiers : opponents;
+        printf("%lld\n", max - min);
+    }
+
+    return 0;
+}
+*/
