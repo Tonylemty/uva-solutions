c412af668487cadb38895f58c9dc8e68f23716ce
Tonylemty <chengshinchung31@gmail.com>
2024-06-23 10:08:50 +0800

    Rotating Sentences

--- 50-stars/uva490.c
+++ 50-stars/uva490.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
+#define MAX 101
 
 int main() {
 
-    char string[101][101] = {0};
-    int i, j, maxLen = 0, row = 0;
+    int i, j;
+    int row = 0, maxLen = 0;
+    char string[MAX][MAX];
 
     while (gets(string[row])) {
 
-        if (strlen(string[row]) > maxLen) {
+        if (strlen(string[row]) > maxLen)
             maxLen = strlen(string[row]);
-        }
+        
         row++;
     }
 
     for (i = 0; i < maxLen; i++) {
         for (j = row - 1; j >= 0; j--) {
-
-            if (i >= strlen(string[j])) {
+            if (i >= strlen(string[j]))
                 printf(" ");
-            }
-            else {
+            else
                 printf("%c", string[j][i]);
-            }
         }
         printf("\n");
     }
-    
+
+    return 0;
 }
